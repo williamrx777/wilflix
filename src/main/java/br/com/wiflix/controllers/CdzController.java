c31633712a24d620d5c4817e7a50b918c864073e
william rodrigues <williamr723@gmail.com>
2023-09-08 16:01:50 -0300

    implementando swagger na api cdz

--- src/main/java/br/com/wiflix/controllers/CdzController.java
+++ src/main/java/br/com/wiflix/controllers/CdzController.java
@@ -3,6 +3,8 @@ package br.com.wiflix.controllers;
 import br.com.wiflix.dtos.CdzDTO;
 import br.com.wiflix.entities.Cdz;
 import br.com.wiflix.repositories.CdzRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,25 +16,27 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/cdz")
+@Tag(name = "cdz_episodios")
 public class CdzController {
 
     @Autowired
     private CdzRepository repository;
-
+    @Operation(summary = "Buscar todos os episodios")
     @GetMapping
     public ResponseEntity findAll(){
         var listar = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(listar);
     }
-
+    @Operation(summary = "publicar  episodio")
     @PostMapping
     public ResponseEntity post(@RequestBody @Valid CdzDTO cdzDTO){
         var postar = new Cdz();
         BeanUtils.copyProperties(cdzDTO, postar);
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postar));
     }
-
+    @Operation(summary = "Buscar episodio")
     @GetMapping("/{id}")
     public ResponseEntity getOne(@PathVariable Long id){
         Optional<Cdz> getOne = repository.findById(id);
@@ -42,7 +46,7 @@ public class CdzController {
             return ResponseEntity.status(HttpStatus.OK).body(getOne);
         }
     }
-
+    @Operation(summary = "Atualiza episodio")
     @PutMapping("/{id}")
     public ResponseEntity update(@PathVariable Long id, @RequestBody @Valid CdzDTO cdzDTO){
         Optional<Cdz> update = repository.findById(id);
@@ -54,7 +58,7 @@ public class CdzController {
             return ResponseEntity.status(HttpStatus.OK).body(episodio);
         }
     }
-
+    @Operation(summary = "Deletar episodio")
     @DeleteMapping("/{id}")
     public ResponseEntity delete (@PathVariable Long id){
         Optional<Cdz> delete = repository.findById(id);
